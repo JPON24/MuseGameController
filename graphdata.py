1f83c20bc84d9cc110b7f0fd0cc6fae25f8ae2ab
JPON24 <jpon24@shaw.ca>
2026-08-16 19:50:00 -0600

    starting to learn

--- graphdata.py
+++ graphdata.py
@@ -3,6 +3,7 @@ import constants
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import mne
 
 df = pd.read_csv(constants.FILE_NAME)
 
@@ -14,6 +15,5 @@ initial_time = rotated_arr[0][-1]
 
 rotated_arr[0] -= initial_time
 
-for i in range (5):
-    plt.plot(rotated_arr[0], rotated_arr[i + 1])
+plt.plot(rotated_arr[0], rotated_arr[2])
 plt.show()
--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import constants
 
-from muselsl import stream, list_muses, record
+import muselsl as lsl
+from pylsl import StreamInlet, resolve_streams
+
+import mne
 import os
 
+print(dir(lsl))
 
 # muses = list_muses()
 # stream(muses[0]['address'])
 
-os.remove(constants.FILE_NAME)
-record(duration=10, filename=constants.FILE_NAME)
+def write_data():
+    os.remove(constants.FILE_NAME)
+    lsl.record(duration=10, filename=constants.FILE_NAME)
 
+def realtime_stream():
+
+    while True:
+        pass
+
+# realtime_stream()
 print('End program')
